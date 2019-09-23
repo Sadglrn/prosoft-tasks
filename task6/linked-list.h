36f7d944b2316596bf6abb2a6118df19a490376d
Sadglrn <evgenij.stepancenko@gmail.com>
2019-09-23 17:54:06 +0700

    Update linked-list.h

--- task6/linked-list.h
+++ task6/linked-list.h
@@ -1,104 +1,34 @@
-#include "linked-list.h"
-#include <stdlib.h>
+/*
+  linked-list.h
+  Generic linked list type
+*/
+
+// Node
+typedef struct _sNode
+{
+  // void pointer to data
+  void *data;
+  // node pointer
+  struct _sNode* nextNode;
+} _sNode;
+
+typedef _sNode* sNode;
+
+// Linked list
+typedef struct _sLinkedList
+{
+  sNode first;
+} _sLinkedList;
+
+typedef _sLinkedList* sLinkedList;
 
 // Creation linked list
-sLinkedList InitLinkedList(void)
-{
-  sLinkedList linkedList;
-
-  if (((linkedList = (sLinkedList)malloc(sizeof(_sLinkedList))) == NULL))
-    return NULL;
-
-  linkedList->first = NULL;
-
-  return linkedList;
-}
-
+sLinkedList InitLinkedList(void);
 // Add new node to linked list
-int push(sLinkedList linkedList, const void* data, int dataSize)
-{
-  sNode node;
-  sNode tmp;
-
-  // Allocate memory for node
-  if (((node = (sNode)malloc(sizeof(_sNode)))) == NULL)
-    return -1;
-
-  // Adding the first element
-  if (linkedList->first == NULL)
-  {
-    linkedList->first = node;
-    node->nextNode = NULL;
-    node->data = malloc(sizeof(dataSize));
-  }
-
-  // Adding the rest elements
-  else
-  {
-    tmp = linkedList->first;
-    while(tmp->nextNode != NULL)
-      tmp = tmp->nextNode;
-
-    tmp->nextNode = node;
-    node->nextNode = NULL;  
-    node->data = malloc(sizeof(dataSize));
-  }
-
-  // Copy contents of data to newly allocated memory
-  for (int i = 0; i < dataSize; i++)
-  *(char*)(node->data + i) = *(char*)(data + i);
-
-  return 0;
-}
-
-// Get a node from linked list
-void* pop(sLinkedList linkedList)
-{
-  sNode tmp = NULL;
-
-  if (linkedList->first != NULL)
-  {
-    tmp = linkedList->first;
-    while(tmp->nextNode != NULL)
-      tmp = tmp->nextNode;
-  }
-
-  return tmp;
-}
-
-// Delete a linked list
-void RemoveLinkedList(sLinkedList linkedList)
-{
-  sNode tmp;
-
-  if (linkedList->first != NULL)
-  {
-    tmp = linkedList->first;
-    free(linkedList->first->data);
-    free(linkedList->first);
-
-    while (tmp->nextNode != NULL)
-    {
-      tmp = tmp->nextNode;
-      free(tmp->data);
-      free(tmp);
-    }
-  }
-
-  free(linkedList);
-}
-
-// Print data from linked list
-void printDataFromList(sLinkedList linkedList, void (*fptr)(void *))
-{
-  sNode tmp = NULL;
-
-  tmp = linkedList->first;
-  while (tmp->nextNode != NULL) 
-  { 
-      (*fptr)(tmp->data); 
-       tmp = tmp->nextNode; 
-  } 
-}
-
-
+int push(sLinkedList, const void *data, int dataSize);
+// Get last node from linked list
+void* pop(sLinkedList);
+// Delete linked list
+void RemoveLinkedList(sLinkedList);
+// Display data from linked list
+void printDataFromList(sLinkedList, void (*fptr)(void *));
